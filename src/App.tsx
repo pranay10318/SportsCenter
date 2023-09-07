b359aecf25dc4ecd1d725414e08d9b525984f2c5
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-09-07 11:38:54 +0530

    bug fixes-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes";
-import { PreferencesProvider } from "./context/preferences/context";
 
 const App = () => {
   // const { theme } = useContext(ThemeContext);
   return (
     <div className={`h-screen w-full mx-auto py-2`}>
-      <PreferencesProvider>
         <RouterProvider router={router} />
-      </PreferencesProvider>
     </div>
   );
 };
