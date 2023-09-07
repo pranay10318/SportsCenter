e27af4130260f2f4d6c3a4bff6acb339fc9fd74e
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-09-07 11:46:15 +0530

    bug fixes-2

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import SigninForm from "./SigninForm";
-import { AuthProvider } from "../../context/AuthContext";
 
-// Dialogue 1: Let's define the Signin component
 const Signin: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
@@ -10,14 +8,9 @@ const Signin: React.FC = () => {
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
           Sign in
         </h1>
-        <AuthProvider>
-          <SigninForm />
-        </AuthProvider>
-        
+        <SigninForm />        
       </div>
     </div>
   );
 };
-
-// Dialogue 3: And finally, we've to export the component
 export default Signin;
